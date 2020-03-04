78c53b3cf29d9131a0666363cf09a58b246a27f5
Brently Bazor <brently.bazor@gmail.com>
2020-03-04 08:30:03 -0500

    added new container component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Container from './components/Container.js'
+import ContainerKB from './components/ContainerKB'
 
 function App() {
   return (
-    <Container />
+    <>
+      <Container />
+      <Container />
+    </>
   );
 }
 
