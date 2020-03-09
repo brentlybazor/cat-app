0a7a5f1a990b9a7c1ad68c8afec7d46a90b40d82
Brently Bazor <brently.bazor@gmail.com>
2020-03-08 22:26:44 -0400

    removed erronous file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Container from './components/Container.js'
-import Containerkb from './components/Containerkb.js'
 
 function App() {
   return (
