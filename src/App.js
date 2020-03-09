5577ad1719307da69027e08a4d01181cb1ee3784
Brently Bazor <brently.bazor@gmail.com>
2020-03-08 20:48:59 -0400

    added dog image

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CatImageContainer from './components/CatImageContainer'
+import DogImageContainer from './components/DogImageContainer'
 
 function App() {
   return (
     <>
       <CatImageContainer />
+      <DogImageContainer />
     </>
   );
 }
